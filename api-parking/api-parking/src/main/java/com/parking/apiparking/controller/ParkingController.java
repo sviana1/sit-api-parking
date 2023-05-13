b20de957b85b1e90705b1c3cfd63d5e5157bf0fa
samuel viaña <svianal1@upao.edu.pe>
2023-05-13 00:48:44 -0500

    feat: crear la clase ParkingController- implementado los metodos de getAllCars y addCar

--- api-parking/api-parking/src/main/java/com/parking/apiparking/controller/ParkingController.java
+++ api-parking/api-parking/src/main/java/com/parking/apiparking/controller/ParkingController.java
@@ -1,4 +1,34 @@
 package com.parking.apiparking.controller;
+
+import com.parking.apiparking.entities.Car;
+import com.parking.apiparking.services.ParkingService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/parking") // url --> http:/dominio/parking
 public class ParkingController {
 
+    //Inyeccion de dependencias
+
+    private final ParkingService parkingService;
+
+    public ParkingController(ParkingService parkingService){
+        this.parkingService = parkingService;
+    }
+@GetMapping("/cars")
+    public ResponseEntity<List<Car>> getAllCars(){
+        return ResponseEntity.ok(this.parkingService.getAllCars());
+    }
+@PostMapping("/cars")
+    public ResponseEntity<Car> addCar(Car car){
+        this.parkingService.addCar(car);
+        return new ResponseEntity<>(car, HttpStatus.CREATED);
+    }
 }
--- api-parking/api-parking/src/main/java/com/parking/apiparking/services/ParkingService.java
+++ api-parking/api-parking/src/main/java/com/parking/apiparking/services/ParkingService.java
@@ -1,10 +1,11 @@
 package com.parking.apiparking.services;
 
 import com.parking.apiparking.entities.Car;
+import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
-
+@Service
 public class ParkingService {
     private List<Car> parkingLot;
 

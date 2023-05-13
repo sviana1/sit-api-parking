6f62b39eb4ca082551b76648b6f5bb5f7e84256f
samuel viaña <svianal1@upao.edu.pe>
2023-05-13 01:02:15 -0500

    feat: Se realizo la prueba de los metodos GET y POST del API con Postman

--- api-parking/api-parking/src/main/java/com/parking/apiparking/controller/ParkingController.java
+++ api-parking/api-parking/src/main/java/com/parking/apiparking/controller/ParkingController.java
@@ -4,10 +4,7 @@ import com.parking.apiparking.entities.Car;
 import com.parking.apiparking.services.ParkingService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -27,7 +24,7 @@ public class ParkingController {
         return ResponseEntity.ok(this.parkingService.getAllCars());
     }
 @PostMapping("/cars")
-    public ResponseEntity<Car> addCar(Car car){
+    public ResponseEntity<Car> addCar(@RequestBody Car car){
         this.parkingService.addCar(car);
         return new ResponseEntity<>(car, HttpStatus.CREATED);
     }

565ca13b2e92bccde9332811bfdaa5c2102b7556
samuel viaña <svianal1@upao.edu.pe>
2023-05-13 00:14:36 -0500

    feat: Crear la identidad Car

--- api-parking/api-parking/src/main/java/com/parking/apiparking/entities/Car.java
+++ api-parking/api-parking/src/main/java/com/parking/apiparking/entities/Car.java
@@ -1,4 +1,40 @@
 package com.parking.apiparking.entities;
 
+import java.time.LocalDateTime;
+
 public class Car {
+    private String licencePlate;
+
+    private String color;
+
+    private LocalDateTime entryTime;
+
+    public Car(String licencePlate, String color) {
+        this.licencePlate = licencePlate;
+        this.color = color;
+    }
+
+    public String getLicencePlate() {
+        return licencePlate;
+    }
+
+    public void setLicencePlate(String licencePlate) {
+        this.licencePlate = licencePlate;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public LocalDateTime getEntryTime() {
+        return entryTime;
+    }
+
+    public void setEntryTime(LocalDateTime entryTime) {
+        this.entryTime = entryTime;
+    }
 }
